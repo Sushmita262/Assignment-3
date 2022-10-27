6415349de564ec39e2e6bc72935cdd16fa2de280
Sushmita262 <43111026+Sushmita262@users.noreply.github.com>
2022-10-27 21:45:25 +0600

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,4 @@
+items = ["cat", "Dog", "Books", "Rabbit","Doraemon", 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8 ]
+print(items [5: ])
+New_items = items[11] + items[17]
+print(New_items)
